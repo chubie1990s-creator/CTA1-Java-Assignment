4dac9414bb819c3fbf51b858c33172352300fba5
chubie1990s-creator <chubie1990s@gmail.com>
2026-05-10 22:27:49 -0500

    Add files via upload

---
+++ CTA1.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("First Name: John");
+        System.out.println("Last Name: Smith");
+        System.out.println("Street Address: 123 Main Street");
+        System.out.println("City: Houston");
+        System.out.println("Zip Code: 77001");
+
+    }
+}
